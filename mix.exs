2b28760da99eac9b92d2146efdc04e83d24fffa7
Victor Felder <victor@draft.li>
2023-05-03 14:42:09 +0200

    chore: release v1.0.0-rc3

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Tezex.MixProject do
   use Mix.Project
 
-  @version "1.0.0-rc2"
+  @version "1.0.0-rc3"
   @url_docs "http://hexdocs.pm/tezex"
   @url_github "https://github.com/objkt-com/tezex"
 
