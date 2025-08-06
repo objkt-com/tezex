9586fe9c392e124d4a771e4dd44fe5aac70a2a62
victor felder <victor@draft.li>
2025-08-06 15:01:24 +0200

    feat: release v3.2.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Tezex.MixProject do
   use Mix.Project
 
-  @version "3.1.0"
+  @version "3.2.0"
   @url_docs "http://hexdocs.pm/tezex"
   @url_github "https://github.com/objkt-com/tezex"
 
